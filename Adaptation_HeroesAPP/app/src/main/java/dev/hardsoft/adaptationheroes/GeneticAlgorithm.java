cd3a9d2eff6eb5c16a12b866e0e3a799ad2e9b19
AllenRR <allen.ramirezramirez@ucr.ac.cr>
2022-10-02 14:02:53 -0600

    Implemented geneticAlgorithm()

--- Adaptation_HeroesAPP/app/src/main/java/dev/hardsoft/adaptationheroes/GeneticAlgorithm.java
+++ Adaptation_HeroesAPP/app/src/main/java/dev/hardsoft/adaptationheroes/GeneticAlgorithm.java
@@ -2,13 +2,8 @@ package dev.hardsoft.adaptationheroes;
 
 import android.os.Build;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+//import java.util.Arrays;
 import java.util.Random;
-import java.util.stream.Stream;
 
 public class GeneticAlgorithm {
     //public  String adaptabilityKey = "Adaptability";
@@ -22,78 +17,70 @@ public class GeneticAlgorithm {
     public int worstAdaptability = 0;
     public int primeIndex = 0;
     public int worstIndex = 0;
-    //static Map<String, Integer> pool = new HashMap<String, Integer>();
-    //static Map<String, Integer> organism = new HashMap<String, Integer>();
-    //static Map<String, Integer> prime = new HashMap<String, Integer>();
+    Random random = new Random(); // Gets random element from pool
     int[][] pool = new int[poolSize][2];
     int[][] organism = new int[organismCards+1][2];
     int[][] prime = new int[organismCards+1][2];
-    int[][][] generation = new int[populationSize][organismCards][2];
-    int[][][] newGeneration = new int[populationSize][organismCards][2];
+    int[][][] generation = new int[populationSize][organismCards+1][2];
+    int[][][] newGeneration = new int[populationSize][organismCards+1][2];
 
     public int[][] geneticAlgorithm(int[][] p) {
         pool = p;
-        //List<Map<String, Integer>> generation = new ArrayList<Map<String, Integer>>();
-
         for (int n = 0; n < cycles; n++) {
-            Arrays.fill(generation, null);
-            Random random = new Random(); // Gets random element from pool
-            anyOrganism = random.nextInt(poolSize);
+            //Arrays.fill(newGeneration, null);
+            newGeneration = new int[populationSize][organismCards+1][2];
             for (int i = 0; i < populationSize; i++) { // Generate population
-                Arrays.fill(organism, null);
-
-                for (int j = 1; j < organismCards; j++) { // Make organism
+                //Arrays.fill(organism, null);
+                organism = new int[organismCards+1][2];
+                for (int j = 1; (j <= organismCards) && (n==0); j++) { // Make organism
+                    anyOrganism = random.nextInt(poolSize);
                     organism[0][0] = -1;
                     organism[0][1] += pool[anyOrganism][1]; // Calculate the adaptability value
                     organism[j][0] = pool[anyOrganism][0];
                     organism[j][1] = pool[anyOrganism][1];
-
-                    if(organism[0][1] > primeAdaptability){
-                        prime = organism;
-                        primeAdaptability = organism[0][1];
-                        primeIndex = j;
-                    }else{
-                        if(organism[0][1] < worstAdaptability) {
-                            worstAdaptability = organism[0][1];
-                            worstIndex = j;
-                        }
-                    }
-                    anyOrganism = random.nextInt(poolSize);
+                    //anyOrganism = random.nextInt(poolSize);
                 }
-
+                if(organism[0][1] > primeAdaptability){
+                    prime = organism;
+                    primeAdaptability = organism[0][1];
+                    primeIndex = i;
+                }else if(organism[0][1] < worstAdaptability) {
+                    worstAdaptability = organism[0][1];
+                    worstIndex = i;
+                }
                 generation[i] = organism;
             }
             // Reproduce the population, generates new individuals
-            for (int j = 1; j < populationSize; j++) {
-                for(int k = 0; k < organismCards; k++) { // Make new individual (best parent)
-                    if(k<5) {
-                        newGeneration[0][k][0] = generation[primeIndex][k][0];
-                        newGeneration[0][k][1] = generation[primeIndex][k][1];
+            for (int j = 0; j < populationSize; j++) {
+                for(int k = 1; k <= organismCards; k++) { // Make new individual (best parent)
+                    anyOrganism = random.nextInt(populationSize);
+                    if(k-1<(int)organismCards/2) {
+                        if(j==0){
+                            newGeneration[0][k][0] = generation[primeIndex][k][0];
+                            newGeneration[0][k][1] = generation[primeIndex][k][1];
+                            newGeneration[j][0][1] += generation[primeIndex][k][1]; // Calculate the adaptability value
+                        }else if(j==1){
+                            newGeneration[1][k][0] = generation[worstIndex][k][0];
+                            newGeneration[1][k][1] = generation[worstIndex][k][1];
+                            newGeneration[j][0][1] += generation[worstIndex][k][1]; // Calculate the adaptability value
+                        }else{
+                            newGeneration[j][k][0] = generation[anyOrganism][k][0];
+                            newGeneration[j][k][1] = generation[anyOrganism][k][1];
+                            newGeneration[j][0][1] += generation[anyOrganism][k][1]; // Calculate the adaptability value
+                        }
                     }else{
-                        anyOrganism = random.nextInt(poolSize);
-                        newGeneration[0][k][0] = generation[anyOrganism][k][0];
-                        newGeneration[0][k][1] = generation[anyOrganism][k][1];
-                    }
-                }
-                for(int k = 0; k < organismCards; k++) { // Make new individual (worst parent)
-                    if(k<5) {
-                        newGeneration[0][k][0] = generation[worstIndex][k][0];
-                        newGeneration[0][k][1] = generation[worstIndex][k][1];
-                    }else{
-                        anyOrganism = random.nextInt(poolSize);
-                        newGeneration[0][k][0] = generation[anyOrganism][k][0];
-                        newGeneration[0][k][1] = generation[anyOrganism][k][1];
-                    }
-
-                }
-                for(int k = 0; k < organismCards; k++) { // Make new individual (average parents)
-                    if(k<5) {
-                        newGeneration[j][k][0] = generation[j][k][0];
-                        newGeneration[j][k][1] = generation[j][k][1];
-                    }else{
-                        anyOrganism = random.nextInt(poolSize);
                         newGeneration[j][k][0] = generation[anyOrganism][k][0];
                         newGeneration[j][k][1] = generation[anyOrganism][k][1];
+                        newGeneration[j][0][1] += generation[anyOrganism][k][1]; // Calculate the adaptability value
+                    }
+                    newGeneration[j][0][0] = -1;
+                    if(newGeneration[j][0][1] > primeAdaptability){
+                        prime = newGeneration[j];
+                        primeAdaptability = newGeneration[j][0][1];
+                        primeIndex = j;
+                    }else if(newGeneration[j][0][1] < worstAdaptability) {
+                        worstAdaptability = newGeneration[j][0][1];
+                        worstIndex = j;
                     }
                 }
             }
--- Adaptation_HeroesAPP/app/src/main/java/dev/hardsoft/adaptationheroes/MainActivity.java
+++ Adaptation_HeroesAPP/app/src/main/java/dev/hardsoft/adaptationheroes/MainActivity.java
@@ -21,30 +21,32 @@ public class MainActivity extends AppCompatActivity {
         //Map<String, Integer> pool = new HashMap<String, Integer>();
         //Map<String, Integer> prime = new HashMap<String, Integer>();
         // Change Card id to int
-        int poolSize = 5;
+        /**int poolSize = 5;
         int organismCards = 2;
         int[][] pool = new int[poolSize][2];
         int[][] prime = new int[organismCards+1][2];
 
         GeneticAlgorithm gA = new GeneticAlgorithm();
+        prime = gA.geneticAlgorithm(pool);*/
+
+        System.out.println("Start");
+        int poolSize = 5;
+        int organismCards = 3;
+        int[][] pool = new int[poolSize][2];
+        int[][] prime = new int[organismCards+1][2];
+
+        // Populate test values
+        for(int i = 0; i<poolSize; i++){
+            pool[i][0] = i;
+            pool[i][1] = i;
+        }
+
+        GeneticAlgorithm gA = new GeneticAlgorithm();
         prime = gA.geneticAlgorithm(pool);
+
+        System.out.println("Prime:");
+        for (int i = 0; i < prime.length; i++)
+            for (int j = 0; j < prime[i].length; j++)
+                System.out.print(prime[i][j] + " ");
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
