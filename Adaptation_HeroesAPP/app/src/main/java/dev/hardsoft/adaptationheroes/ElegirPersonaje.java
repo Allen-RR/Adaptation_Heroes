e64fbdf61a1650327b6df4daa092f16575972e3e
Steven_programmer <steven.josspa@gmail.com>
2022-10-02 00:33:14 -0600

    Interfaz Elige tu personaje

---
+++ Adaptation_HeroesAPP/app/src/main/java/dev/hardsoft/adaptationheroes/ElegirPersonaje.java
@@ -0,0 +1,14 @@
+package dev.hardsoft.adaptationheroes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ElegirPersonaje extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_elegir_personaje);
+    }
+}
